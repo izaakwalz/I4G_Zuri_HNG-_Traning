56cef7e50070994d600dcc68f4e59781e53f4867
izaakwalz <iblack.xyz@gmail.com>
2021-05-06 11:06:26 +0100

    added routes to server

--- backend/server.js
+++ backend/server.js
@@ -1,18 +1,22 @@
 const express = require('express');
 const dotenv = require('dotenv');
 const connectDB = require('./config/db');
-const errorHandler = require('./middlewares/error-handler');
+const { notFound, errorHandler } = require('./middlewares/error-handler');
 
 dotenv.config();
 
 const app = express();
-
+// body parser
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 
+// API routes
+app.use('/api/v1', require('./routes/index.routes'));
+
+app.use(notFound);
 app.use(errorHandler);
 
-const PORT = process.env.PORT || 2021;
+const PORT = process.env.PORT || 4000;
 
 app.listen(PORT, async () => {
   await connectDB();
